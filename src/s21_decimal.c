2e9a4ec6f2ace9f1b7766e4830841e971b4edbc7
Ladonna Harclay <ladonnah@su-f1.surgut.21-school.ru>
2023-10-12 01:24:10 +0500

    +global_carry between s21_uint96_t with add()

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -10,8 +10,8 @@
 s21_uint96_t add_uint96_v1(s21_uint96_t x, s21_uint96_t y)
 {
     s21_uint96_t result = {0};
-    unsigned int carry = 0;
-    int i = sizeof(x.value)/sizeof(x.value[0]);
+    unsigned carry = 0;
+    unsigned short i = sizeof(x.value)/sizeof(x.value[0]);
     /* Start from LSB */
     while(i--)
     {
@@ -25,23 +25,33 @@ s21_uint96_t add_uint96_v1(s21_uint96_t x, s21_uint96_t y)
     return result;
 }
 
-s21_uint96_t add_uint96_v2(s21_uint96_t x, s21_uint96_t y)
-{
+uint32_t s21_add_uint32_t(uint32_t *x, uint32_t *y) {
+      unsigned global_carry = 0;
+      unsigned long carry = 0;
+      while(*y != 0x00)
+      {
+        /*Выставить флаг под разрядами с установленными битами.*/
+        carry	= (*x & *y);
+        /*Снять с первого слагаемого биты, разряд по которым уже учтен.*/
+        *x = *x ^ *y;
+        /*Перенос переводится в старший разряд.*/
+        *y = (carry << 1);
+        if ((carry << 1) > UINT32_MAX)
+        global_carry = 1;
+      }
+      return global_carry;
+}
+
+s21_uint96_t add_uint96_v2(s21_uint96_t x, s21_uint96_t y) {
     s21_uint96_t result = {0};
-    int i = sizeof(x.value)/sizeof(x.value[0]);
+    short i = sizeof(s21_uint96_t) / sizeof(uint32_t);
+    unsigned carry = 0;
     /* Start from LSB */
     while(i--)
     {
-      unsigned carry = 0;
-      while(y.value[i] != 0x00)
-      {
-        /*Выставить флаг под разрядами с установленными битами.*/
-        carry	= (x.value[i] & y.value[i]);
-        /*Снять с первого слагаемого биты, разряд по которым уже учтен.*/
-        x.value[i] = x.value[i] ^ y.value[i];
-        /*Перенос переводится в старший разряд.*/
-        y.value[i] = (carry << 1);
-	    }
+      carry = s21_add_uint32_t(&x.value[i], &y.value[i]);
+      if (carry)
+        carry = s21_add_uint32_t(&y.value[(i - 1) >= 0 ? i - 1 : 0], &carry);
     }
     result = x;
     return result;
@@ -49,7 +59,7 @@ s21_uint96_t add_uint96_v2(s21_uint96_t x, s21_uint96_t y)
 
 void print_uint96(char *str, s21_uint96_t x)
 {
-    printf("%s = %08x%08x%08x\r\n", str, x.value[0], x.value[1], x.value[2]);
+    printf("%s = %010x%010x%010x\r\n", str, x.value[0], x.value[1], x.value[2]);
 }
 
 void bin_print(unsigned int length, void *pointer, int options) {
@@ -71,40 +81,45 @@ void bin_print(unsigned int length, void *pointer, int options) {
 }
 
 int main() {
-  // s21_decimal d = {0};
-  
+  s21_uint96_t x = {{0, UINT32_MAX, UINT32_MAX}};
+
+  s21_uint96_t y = {{0, 0, 1}};
+
+  s21_uint96_t result = add_uint96_v2(x, y);
+
+  print_uint96("result:", result);
   // Вариант 2 - "Все число сразу"
 
-  unsigned summand = 15;
-  printf("  summand: ");
-  bin_print(32, &summand, 1);
+  // unsigned summand = UINT32_MAX;
+  // printf("  summand: ");
+  // bin_print(32, &summand, 1);
 
-  unsigned addend = 1; 
-  printf("   addend: ");
-  bin_print(32, &addend, 1);
+  // unsigned addend = 1; 
+  // printf("   addend: ");
+  // bin_print(32, &addend, 1);
   
-  /*Перенос.*/
-	unsigned carry = 0x00;
+  // /*Перенос.*/
+	// unsigned carry = 0x00;
 
-	/*Итерировать до тех пор, пока не закончится перенос на старший разряд.*/
-	while(addend != 0x00) {
-		/*Выставить флаг под разрядами с установленными битами.*/
-    carry = (summand & addend);
-    printf("    carry: ");
-    bin_print(32, &carry, 1);
+	// /*Итерировать до тех пор, пока не закончится перенос на старший разряд.*/
+	// while(addend != 0x00) {
+	// 	/*Выставить флаг под разрядами с установленными битами.*/
+  //   carry = (summand & addend);
+  //   printf("    carry: ");
+  //   bin_print(32, &carry, 1);
 
-		/*Снять с первого слагаемого биты, разряд по которым уже учтен.*/
-		summand	= summand ^ addend;
-    printf("summand_a: ");
-    bin_print(32, &summand, 1);
+	// 	/*Снять с первого слагаемого биты, разряд по которым уже учтен.*/
+	// 	summand	= summand ^ addend;
+  //   printf("summand_a: ");
+  //   bin_print(32, &summand, 1);
 
-    /*Перенос переводится в старший разряд.*/
-		addend = (carry << 1);
-    printf(" addend_a: ");
-    bin_print(32, &addend, 1);
-	}
+  //   /*Перенос переводится в старший разряд.*/
+	// 	addend = (carry << 1);
+  //   printf(" addend_a: ");
+  //   bin_print(32, &addend, 1);
+	// }
 
-  printf("result: %u\n", summand);
+  // printf("result: %u\n", summand);
 
   // Вариант 1 - "В столбик"
   // unsigned c1 = 255;
