33d1b53ae98a54e301b68893c9ce0d8639120fff
Ronin Shawanda <roninsha@student.21-school.ru>
2024-01-12 16:31:08 +0500

    Fixed valgrind's errors

--- src/function/s21_mul_lazy.c
+++ src/function/s21_mul_lazy.c
@@ -6,52 +6,41 @@
 
 int s21_mul_lazy(s21_decimal_lazy *value_1, s21_decimal_lazy *value_2,
                  s21_decimal_lazy *result) {
+  int error = 0;
+  uint8_t *temp = NULL;
+  s21_decimal_lazy tmp = {0}, empty = {0};
 
- int error = 0;
- uint16_t i = 0, j = 0;
-
- uint8_t *temp = realloc(result->mantissa, value_1->size + value_2->size);
- if (temp) {
+  temp = realloc(result->mantissa, value_1->size + value_2->size);
+  if (temp != NULL) {
     result->mantissa = temp;
     result->size = value_1->size + value_2->size;
     memset(result->mantissa, 0, result->size);
- } else
+    error = !!(s21_lazy_to_lazy_cp(result, &tmp) +
+               s21_lazy_to_lazy_cp(result, &empty));
+  } else
     error = 1;
 
- if (!error) {
+  if (!error) {
     uint16_t res = 0;
     uint8_t carry = 0;
-    s21_decimal_lazy tmp = {0}, empty = {0};
-    s21_lazy_to_lazy_cp(result, &tmp);
-    s21_lazy_to_lazy_cp(result, &empty);
 
-    while (i < value_1->size) {
-        while (j < value_2->size) {
-            res = (uint16_t) *(value_1->mantissa + i) * *(value_2->mantissa + j) + carry;
-            carry =  res >> sizeof(uint8_t) * CHAR_BIT;
-            *(tmp.mantissa + i + j) = (uint8_t) res;
-            *(tmp.mantissa + i + j + 1) = carry;
-            j++;
-        }
-        s21_add_lazy(&tmp, result, result);
-        s21_lazy_to_lazy_cp(&empty, &tmp);
-        j = carry = 0;
-        i++;
+    for (uint16_t i = 0; i < value_1->size && !error; i++) {
+      for (uint16_t j = 0; j < value_2->size && !error; j++) {
+        res = (uint16_t)(*(value_1->mantissa + i) * *(value_2->mantissa + j));
+        res += carry;
+        *(tmp.mantissa + i + j) = (uint8_t)res;
+
+        carry = res >> sizeof(uint8_t) * CHAR_BIT;
+        *(tmp.mantissa + i + j + 1) = carry;
+      }
+      error = !!(s21_add_lazy(&tmp, result, result) +
+                 s21_lazy_to_lazy_cp(&empty, &tmp));
+      carry = 0;
     }
 
-    // Если остался перенос, доаллоцируем себе массив
-   // if (carry)
-   //    *(result->mantissa + i + j) += (uint8_t) carry;
-    //     result->size++;
-    //     uint8_t *temp = realloc(result->mantissa, result->size);
-    //     if (temp) {
-    //     result->mantissa = temp;
-    //     result->mantissa[result->size - 1] = carry;
-    //     } else
-    //     error = 1;
-    // }
- }
- 
+    if (tmp.mantissa != NULL) free(tmp.mantissa);
+    if (empty.mantissa != NULL) free(empty.mantissa);
+  }
 
- return error;
+  return error;
 }
--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -10,19 +10,20 @@
 void s21_decimal_lazy_print(s21_decimal_lazy* x) {
   uint16_t size = 0;
   while (size < x->size) {
-    printf("%hhu ", *(x->mantissa + size));
+    // printf("%hhu ", *(x->mantissa + size));
+    printf("%hhx ", *(x->mantissa + size));
     size++;
   }
   printf(" exp: %u\n", x->exponent);
 }
 
 int main() {
-  s21_decimal value_1 = {{0x00000000, 0x00000000, 0xffff0000, 0x00000000}};
-  s21_decimal value_2 = {{0x00000000, 0xffff0000, 0x00000000, 0x00000000}};
+  s21_decimal value_1 = {{0x0000ffff, 0x00000000, 0x0000, 0x00000000}};
+  s21_decimal value_2 = {{0x0000ffff, 0x0000, 0x00000000, 0x00000000}};
   // s21_decimal result = {0};
 
-  s21_decimal_lazy x  = {0};
-  s21_decimal_lazy y  = {0};
+  s21_decimal_lazy x = {0};
+  s21_decimal_lazy y = {0};
   s21_decimal_lazy result = {0};
   s21_dec_to_lazy_cp(&value_1, &x);
   s21_dec_to_lazy_cp(&value_2, &y);
@@ -32,9 +33,11 @@ int main() {
   // s21_lazy_normalization(&result, 12);
   // s21_add_lazy(&x, &y, &result);
   s21_mul_lazy(&x, &y, &result);
+  s21_decimal_lazy_print(&x);
+  s21_decimal_lazy_print(&y);
   s21_decimal_lazy_print(&result);
-  free (x.mantissa);
-  free (y.mantissa);
+  free(x.mantissa);
+  free(y.mantissa);
   free(result.mantissa);
 
   return 0;
