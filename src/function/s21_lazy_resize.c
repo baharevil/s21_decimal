21a99ff8731dd13aee57a6c6a9bcfe25d3abc1a4
Igor Baharev <48806222+baharevil@users.noreply.github.com>
2024-01-17 15:34:04 +0000

    Fixed: s21_lazy_resize() first validation

--- src/function/s21_lazy_resize.c
+++ src/function/s21_lazy_resize.c
@@ -5,8 +5,9 @@
 uint8_t s21_lazy_resize(s21_decimal_lazy **lazy) {
   uint8_t result = 0;
 
-  result = !((*lazy)->size > 0 && (*lazy)->size < UINT16_MAX);
-  result |= !(*lazy != NULL && (*lazy)->mantissa != NULL);
+  if (*lazy == NULL || (*lazy)->mantissa == NULL || (*lazy)->size < 1 ||
+      (*lazy)->size > UINT16_MAX - 1)
+    result = 1;
 
   if (!result) {
     uint8_t *temp = realloc((*lazy)->mantissa, sizeof(uint8_t) * (*lazy)->size);
