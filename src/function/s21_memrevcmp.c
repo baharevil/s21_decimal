fc3545a53baae2eac36b3bd9ec2fe0506e07f2dc
Igor Baharev <48806222+baharevil@users.noreply.github.com>
2024-01-17 18:39:45 +0000

    Added: s21_memrevcmp() for reverse comparing s21_decimal or s21_decimal_lazy

---
+++ src/function/s21_memrevcmp.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+/*Функция создана и работает приближено к memcmp(), но реверсивно*/
+int s21_memrevcmp(const void *s1, const void *s2, uint16_t size) {
+    int result = 0;
+    const uint8_t *s1_ptr = s1;
+    const uint8_t *s2_ptr = s2;
+
+    if (size == 0) size = 12;
+
+    s1_ptr += size;
+    s2_ptr += size;
+
+    while (size > 0 && !result) {
+      if (*(s1_ptr + size - 1) > *(s2_ptr + size - 1))
+        result = 1;
+      else if (*(s1_ptr + size - 1) < *(s2_ptr + size - 1))
+        result = -1;
+      size--;
+    }
+    return result;
+}
