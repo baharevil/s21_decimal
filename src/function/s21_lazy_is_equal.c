bcb6f3ab5da5d6a38a57badaf2add0e86a041dd6
netzen86 <netzen0su@gmail.com>
2024-01-19 00:48:47 +0500

    added comment

--- src/function/s21_lazy_is_equal.c
+++ src/function/s21_lazy_is_equal.c
@@ -1,17 +1,16 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 #include "s21_decimal.h"
 
-//! BUG: 42 ошибки по valgrind'у
+//! BUG: при значении экспоненты 0x001A0000 0x00030000, число c большей экспоненотой больше 
 int s21_lazy_is_equal(s21_decimal *value_1, s21_decimal *value_2) {
   int result;
   uint8_t exp;
   uint16_t size;
 
   if (value_1->exponent.bits.sign == value_2->exponent.bits.sign) {
-
     exp = value_1->exponent.bits.exponent;
     if (value_1->exponent.bits.exponent < value_2->exponent.bits.exponent) {
       exp = value_2->exponent.bits.exponent;
@@ -32,23 +31,22 @@ int s21_lazy_is_equal(s21_decimal *value_1, s21_decimal *value_2) {
     } else if (value_1_l.size < value_2_l.size) {
       size = value_2_l.size;
       s21_lazy_resize(&value_1_l, size);
-    } else if (value_1_l.size == value_2_l.size)  {
+    } else if (value_1_l.size == value_2_l.size) {
       size = value_1_l.size;
     }
-    
+
     if (&exp != NULL) {
       s21_lazy_normalization(&value_1_l, exp);
       s21_lazy_normalization(&value_2_l, exp);
     }
 
     result = s21_memrevcmp(value_1_l.mantissa, value_2_l.mantissa, size);
-    
+
     if (value_1_l.mantissa != NULL) free(value_1_l.mantissa);
     if (value_2_l.mantissa != NULL) free(value_2_l.mantissa);
   } else if (value_1->exponent.bits.sign < value_2->exponent.bits.sign) {
     result = 1;
-  }
-  else {
+  } else {
     result = -1;
   }
   return result;
