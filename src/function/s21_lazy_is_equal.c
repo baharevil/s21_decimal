cdd51066303f1a1e06f65f068443828dbbd9d91b
netzen86 <netzen0su@gmail.com>
2024-01-19 00:21:28 +0500

    added s21_memrevcmp, fix valgring error, work with mantissa but error 100 1 not fix

--- src/function/s21_lazy_is_equal.c
+++ src/function/s21_lazy_is_equal.c
@@ -1,42 +1,53 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "s21_decimal.h"
 
 //! BUG: 42 ошибки по valgrind'у
 int s21_lazy_is_equal(s21_decimal *value_1, s21_decimal *value_2) {
   int result;
+  uint8_t exp;
+  uint16_t size;
 
   if (value_1->exponent.bits.sign == value_2->exponent.bits.sign) {
-    int exp_min = 2;
-    uint8_t exp = value_1->exponent.bits.exponent;
-    
+
+    exp = value_1->exponent.bits.exponent;
     if (value_1->exponent.bits.exponent < value_2->exponent.bits.exponent) {
       exp = value_2->exponent.bits.exponent;
-      exp_min = 1;
     }
 
-    s21_decimal_lazy value_1_l = {0};
-    s21_decimal_lazy value_2_l = {0};
+    s21_decimal_lazy value_1_l;
+    s21_decimal_lazy value_2_l;
+
+    s21_lazy_init(&value_1_l);
+    s21_lazy_init(&value_2_l);
 
     s21_dec_to_lazy_cp(value_1, &value_1_l);
     s21_dec_to_lazy_cp(value_2, &value_2_l);
 
-    if (exp_min == 1)
+    if (value_1_l.size > value_2_l.size) {
+      size = value_1_l.size;
+      s21_lazy_resize(&value_2_l, size);
+    } else if (value_1_l.size < value_2_l.size) {
+      size = value_2_l.size;
+      s21_lazy_resize(&value_1_l, size);
+    } else if (value_1_l.size == value_2_l.size)  {
+      size = value_1_l.size;
+    }
+    
+    if (&exp != NULL) {
       s21_lazy_normalization(&value_1_l, exp);
-    else
       s21_lazy_normalization(&value_2_l, exp);
+    }
 
-    result = memcmp(value_1_l.mantissa, value_2_l.mantissa, value_1_l.size);
+    result = s21_memrevcmp(value_1_l.mantissa, value_2_l.mantissa, size);
     
     if (value_1_l.mantissa != NULL) free(value_1_l.mantissa);
     if (value_2_l.mantissa != NULL) free(value_2_l.mantissa);
-  } 
-  
-  else if (value_1->exponent.bits.sign < value_2->exponent.bits.sign) {
+  } else if (value_1->exponent.bits.sign < value_2->exponent.bits.sign) {
     result = 1;
-  } 
-  
+  }
   else {
     result = -1;
   }
