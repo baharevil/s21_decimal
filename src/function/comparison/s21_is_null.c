c8e73d45d6e0ba0aafee4342fe3b68529ff33d43
Ronin Shawanda <roninsha@student.21-school.ru>
2024-01-22 19:26:45 +0500

    Changes name

--- src/function/comparison/s21_is_null.c
+++ src/function/comparison/s21_is_null.c
@@ -7,6 +7,6 @@
   @param[in] *decimal Сравниваемое значения
   @return Код ошибки: 0 - FALSE, 1 - TRUE
 */
-uint8_t s21_decimal_is_null(s21_decimal *decimal) {
+uint8_t s21_is_null(s21_decimal *decimal) {
   return (s21_search_msb(decimal) == 0);
 }
