f2ce7d7f8162ae778092936507daab5d6f0b9ac2
Igor Baharev <48806222+baharevil@users.noreply.github.com>
2024-01-22 19:21:14 +0000

    Fixed: s21_sub() ver.2

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -29,89 +29,54 @@ void s21_decimal_print(s21_decimal* x) {
 }
 
 int main() {
-
   s21_decimal value_1 = {{0x00000005, 0x0, 0x0, 0x80000000}};
-  s21_decimal value_2 = {{0x00000002, 0x0, 0x0, 0x00000000}};
-  // s21_decimal dec_result = {0};
+  s21_decimal value_2 = {{0x00000002, 0x0, 0x0, 0x80000000}};
+  s21_decimal dec_result = {0};
 
   // Найди отличия в результатах:
   //----------------- Ver1 -------------------
-  // s21_sub(value_1, value_2, &dec_result);
+  s21_sub(value_1, value_2, &dec_result);
+  printf("sub ver_1: ");
+  s21_decimal_print(&dec_result);
 
   //----------------- Ver2 -------------------
-  // s21_decimal *lvalue = NULL, *rvalue = NULL;
+  s21_decimal *lvalue = NULL, *rvalue = NULL;
+  s21_decimal null = {0};
 
-  // if(s21_is_less_or_equal(value_1, value_2)) {
-  //   lvalue = &value_2;
-  //   rvalue = &value_1;
-  //   dec_result.exponent.bits.sign = 1;
-  // } else {
-  //   lvalue = &value_1;
-  //   rvalue = &value_2;
-  // }
+  /// @bug неправельный результат при выражени -5 - 2
+  /*
+    В даном случае, по текущей версии, получается что
+    lvalue = 2, rvalue = -5, из-за чего постоянно переносится carry,
+    который не равен 0.
+  */
 
-  // uint8_t carry = 0;
-  // uint16_t res = 0;
-  // for (size_t i = 0; i < (size_t) sizeof(s21_uint96_t); i++) {
-  //     res = lvalue->mantissa.bytes[i] - rvalue->mantissa.bytes[i] - carry;
-  //     dec_result.mantissa.bytes[i] = (uint8_t) res;
-  //     carry = ((res >> sizeof(uint8_t) * CHAR_BIT) > 0);
-  // }
+  if (s21_is_less_or_equal(value_1, null) &&
+      s21_is_less_or_equal(value_2, null)) {
+    value_1.exponent.bits.sign = 0;
+    value_2.exponent.bits.sign = 0;
+    dec_result.exponent.bits.sign = 1;
+  }
+
+  if (s21_is_less_or_equal(value_1, value_2)) {
+    lvalue = &value_2;
+    rvalue = &value_1;
+    dec_result.exponent.bits.sign = 1;
+  } else {
+    lvalue = &value_1;
+    rvalue = &value_2;
+  }
+
+  uint8_t carry = 0;
+  uint16_t res = 0;
+  for (size_t i = 0; i < (size_t)sizeof(s21_uint96_t); i++) {
+    res = lvalue->mantissa.bytes[i] - rvalue->mantissa.bytes[i] - carry;
+    dec_result.mantissa.bytes[i] = (uint8_t)res;
+    carry = ((res >> sizeof(uint8_t) * CHAR_BIT) > 0);
+  }
   //------------------------------------------
 
-  // s21_decimal_print(&dec_result);
-
-  // s21_decimal_lazy x = {0};
-  // s21_decimal_lazy y = {0};
-  // s21_decimal_lazy result = {0};
-  // s21_dec_to_lazy_cp(&value_1, &x);
-  // s21_dec_to_lazy_cp(&value_2, &y);
-  // // s21_lazy_to_lazy_cp(&x, &result);
-
-  // // s21_decimal_lazy_print(&result);
-  // // s21_lazy_normalization(&result, 12);
-  // // s21_add_lazy(&x, &y, &result);
-  // printf("x: ");
-  // s21_decimal_lazy_print(&x);
-  // printf("y: ");
-  // s21_decimal_lazy_print(&y);
-
-  // s21_sub_lazy(&x, &y, &result);
-
-  // printf("result: ");
-  // s21_decimal_lazy_print(&result);
-
-  // s21_lazy_to_dec(&result, &dec_result);
-
-  // printf("dec_result: ");
-  // s21_decimal_print(&dec_result);
-
-  // free(x.mantissa);
-  // free(y.mantissa);
-  // free(result.mantissa);
-
-  s21_decimal_lazy value_l_1;
-  s21_decimal_lazy value_l_2;  
-
-  s21_lazy_init(&value_l_1);
-  s21_lazy_init(&value_l_2);
-
-  s21_dec_to_lazy_cp(&value_1, &value_l_1);
-  s21_dec_to_lazy_cp(&value_2, &value_l_2);
-
-  s21_decimal_print(&value_1);
-  s21_decimal_print(&value_2);
-
-  printf("Equal?: %d\n", s21_equal_lazy(&value_l_1, &value_l_2));
-  printf("less?: %d\n", s21_is_less(value_1, value_2));
-  printf("less or equal?: %d\n", s21_is_less_or_equal(value_1, value_2));
-  printf("greater?: %d\n", s21_is_greater(value_1, value_2));
-  printf("greater or equal?: %d\n", s21_is_greater_or_equal(value_1, value_2));
-  printf("equal?: %d\n", s21_is_equal(value_1, value_2));
-  printf("not equal?: %d\n", s21_is_not_equal(value_1, value_2));
-
-  if (value_l_1.mantissa != NULL) free(value_l_1.mantissa);
-  if (value_l_2.mantissa != NULL) free(value_l_2.mantissa);
+  printf("sub ver_2: ");
+  s21_decimal_print(&dec_result);
 
   return 0;
 }
