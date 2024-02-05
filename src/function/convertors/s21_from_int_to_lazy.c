4afe6128ec75e0b7185fdb2e074fa7375de1b612
Igor Baharev <48806222+baharevil@users.noreply.github.com>
2024-02-05 15:53:54 +0000

    Refactoring s21_from_int_to_lazy.c

--- src/function/convertors/s21_from_int_to_lazy.c
+++ src/function/convertors/s21_from_int_to_lazy.c
@@ -11,18 +11,24 @@
   @return 0 - конвертация успешна, 1 - ошибка конвертации
 */
 uint8_t s21_from_int_to_lazy(int src, s21_decimal_lazy *dst) {
-  int status = conv_ok;
-  if (s21_decimal_ptr_is_valid(dst) && s21_decimal_is_valid(dst)) {
-    s21_lazy_resize(dst, 4);
-    if (src < 0) {
-      dst->sign = 1;
-      src = ~src + 1;
-    }
+  int error = conv_ok;
+  // Валидируем реквизиты
+  if (!s21_decimal_ptr_is_valid(dst) || !s21_decimal_is_valid(dst))
+    error = conv_false;
+  // Изменяем размер к 1 int (4 байта)
+  if (!error)
+    error |= s21_lazy_resize(dst, 4);
+  // Обрабатываем знаки
+  if (!error && src < 0) {
+    dst->sign = 1;
+    src = ~src + 1;
+  }
+  // Пишем в мантиссу
+  if (!error) {
     *(int *)dst->mantissa = src;
     uint16_t size = s21_search_msb_lazy(dst);
     size = (size > 0) * size + (size == 0) * 1;
-    s21_lazy_resize(dst, size);
-  } else
-    status = conv_false;
-  return status;
+    error |= s21_lazy_resize(dst, size);
+  }
+  return error;
 }
