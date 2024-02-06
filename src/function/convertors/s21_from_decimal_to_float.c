d3b081b95b12ca231334d4de016108a935f01be6
maryjori <maryjori@student.21-school.ru>
2024-02-06 10:22:21 +0500

    in job

---
+++ src/function/convertors/s21_from_decimal_to_float.c
@@ -0,0 +1,14 @@
+#include "s21_decimal.h"
+
+/*!
+  @ingroup ConverterOperators Преобразователи
+  @brief Конвертирование числа из decimal в float
+
+  @param[in] src Входящее число decimal
+  @param[in] *dst Указатель на float
+  @return 0 - конвертация успешна, 1 - ошибка конвертации
+*/
+int s21_from_decimal_to_float(s21_decimal src, float *dst) {
+  int status = 0;
+  return status;
+}
