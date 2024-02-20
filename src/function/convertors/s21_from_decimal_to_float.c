d1c7344334924cbc874b919af21fa8177959ad0f
Ladonna Harclay <ladonnah@su-f2.surgut.21-school.ru>
2024-02-20 20:57:15 +0500

    Added: s21_from_decimal_to_float(), Deleted: ./test

--- src/function/convertors/s21_from_decimal_to_float.c
+++ src/function/convertors/s21_from_decimal_to_float.c
@@ -1,6 +1,6 @@
-#include "s21_decimal.h"
+#include <math.h>
 
-/// @bug ЗАШЛУШКА ДЛЯ ТЕСТОВ
+#include "s21_decimal.h"
 
 /*!
   @ingroup ConverterOperators Преобразователи
@@ -11,8 +11,38 @@
   @return 0 - конвертация успешна, 1 - ошибка конвертации
 */
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
-  int status = src.exponent.bits.exponent;
-  *dst = 0;
-  status += (int)*dst;
-  return status;
+  int error = conv_ok;
+  double d = 0.0;
+  s21_decimal_lazy tmp = {0};
+
+  // Первичная валидация
+  if (!dst) error = conv_false;
+
+  //Инициализация переменных
+  if (!error)
+    error |= s21_lazy_init(&tmp, &src);
+
+  if (!error) {
+    // Цикл преобразования
+    for (int32_t i = tmp.size - 1; i >= 0; i--) {
+      d *= 0x100;
+      d += *(tmp.mantissa + i);
+    }
+
+    // Приведение экспоненты
+    if (tmp.exponent)
+      d *= pow(10, (double) -(tmp.exponent));
+
+    // Обработка знаков
+    if (tmp.sign)
+      d *= -1;
+
+    // Экспорт
+    *dst = (float) d;
+
+    //Освобождение памяти
+    s21_lazy_destroy(&tmp);
+  }
+
+  return error;
 }
